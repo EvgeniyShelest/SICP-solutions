2d1097fa2d793d47f737dcc12e9dfd7e617181fe
EvgeniyShelest <evgeniy.shelest@anahoret.com>
2013-03-15 00:53:56 +0200

    Improve.

--- task_1_07.rkt
+++ task_1_07.rkt
@@ -1,31 +1,39 @@
 #lang racket
-(define (sqrt-iter1 guess x)
-  (if (good-enough1? guess x)
-      guess
-      (sqrt-iter1 (improve guess x)
-                 x)))
-(define (sqrt-iter2 guess x)
-  (if (good-enough2? guess x)
-      guess
-      (sqrt-iter2 (improve guess x)
-                 x)))
+;декларация условия задачи
+;√x = такое y, что y ≥ 0 и y^2 = x
+;комплексные решения не учитываем
 
-(define (good-enough1? guess x)
-  (< (abs (- (square guess) x)) 1e-3))
+(require rackunit)
 
-(define (good-enough2? guess x)
-  (< (abs (- (improve guess x) guess)) (* guess 1e-6)))
-
-(define (improve guess x)
-  (if (= guess 0.0)
-      1.0
-      (average guess (/ x guess))))
+(define eps 1e-6)
 (define (square x)
   (* x x))
 (define (average x y)
   (/ (+ x y) 2))
+(define (improve guess x)
+  (average guess (/ x guess)))
 
-(sqrt-iter1 900.0 1e6)
-(sqrt-iter2 990.0 1e6)
-(sqrt-iter1 1.0 2)
-(sqrt-iter2 -0.0 2)
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess)) (* guess eps)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      (improve guess x)
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (sqrt-my x)
+  (cond ((< x 0) #f)
+        ((< x 1e-323) 0.0)
+        ((eqv? x +inf.0) +inf.0)
+        (else (sqrt-iter 1.0 x))))
+
+(check-equal? (sqrt-my 9) 3.0)
+(check-equal? (sqrt-my 1e-325) 0.0)
+(check-false (sqrt-my -1))
+(check < (abs (- (sqrt-my 1e6) (sqrt 1e6))) eps)
+(check < (abs (- (sqrt-my 1e-20) (sqrt 1e-20))) eps)
+
+
+
+
