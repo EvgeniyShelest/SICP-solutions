fa0c31454c10e823674ab2a743556874f9b67a10
EvgeniyShelest <evgeniy.shelest@anahoret.com>
2013-03-22 21:29:57 +0200

    task 12

--- task_1_12.rkt
+++ task_1_12.rkt
@@ -1,12 +1,23 @@
 #lang racket
+(require rackunit)
 
-(define (pascal n)
-  (if (< n 1) #f
-             (pascal-row n)))
-(define (pascal-row n)
-  (if (= n 1) (display 1)
-              (display (pascal-row (- n 1)))
-  ))
+(define (pascal-num row index)
+  (cond ((or (< row 1) (< index 1) (> index row)) #f)
+        ((or (= row index) (= index 1)) 1)
+        (else (+ (pascal-num (- row 1) (- index 1)) (pascal-num (- row 1) index)))))
 
-(pascal 2)
-(pascal 1)
+(define (cycle-out row)
+  (define (cycle-in index)
+    (display (pascal-num row index))
+    (display " ")
+    (if (> index 1) (cycle-in (- index 1))
+                    (display "")))
+  (cycle-in row)
+  (display "\n")
+  (if (> row 1) (cycle-out (- row 1))
+                (display "")))
+
+(cycle-out 10)
+(check-eqv? (pascal-num 4 5) #f)
+(check-eqv? (pascal-num 4 -3) #f)
+(check-eqv? (pascal-num 0 0) #f)
