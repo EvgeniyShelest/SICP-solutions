ff2cdf74ae5a403e57a34dbe74d17a1a1546d385
EvgeniyShelest <evgeniy.shelest@anahoret.com>
2013-03-23 09:36:49 +0200

    Some makeup.

--- task_1_12.rkt
+++ task_1_12.rkt
@@ -1,23 +1,24 @@
 #lang racket
 (require rackunit)
-
+;solution
 (define (pascal-num row index)
   (cond ((or (< row 1) (< index 1) (> index row)) #f)
         ((or (= row index) (= index 1)) 1)
         (else (+ (pascal-num (- row 1) (- index 1)) (pascal-num (- row 1) index)))))
 
+;demonstration
 (define (cycle-out row)
   (define (cycle-in index)
     (display (pascal-num row index))
     (display " ")
     (if (> index 1) (cycle-in (- index 1))
-                    (display "")))
+                    (display "\n")))
   (cycle-in row)
-  (display "\n")
   (if (> row 1) (cycle-out (- row 1))
                 (display "")))
-
 (cycle-out 10)
+
+;tests
 (check-eqv? (pascal-num 4 5) #f)
 (check-eqv? (pascal-num 4 -3) #f)
 (check-eqv? (pascal-num 0 0) #f)
